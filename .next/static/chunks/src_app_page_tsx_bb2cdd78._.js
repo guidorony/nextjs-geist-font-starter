7ea1bcc4c1a142b416c45034dddc3c24a3863a30
Guido Rony Choquemaqui Hualla <53020192+guidorony@users.noreply.github.com>
2025-07-17 13:07:04 -0500

    Add .next/static/chunks/src_app_page_tsx_bb2cdd78._.js 1752775623852

---
+++ .next/static/chunks/src_app_page_tsx_bb2cdd78._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_5bd7e202._.js",
+  "static/chunks/node_modules_764f35e4._.js"
+],
+    source: "dynamic"
+});
