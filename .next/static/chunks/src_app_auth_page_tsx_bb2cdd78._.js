ba2a4e6440bffa8c5bdd48aa57ce4b9243cc728c
Guido Rony Choquemaqui Hualla <53020192+guidorony@users.noreply.github.com>
2025-07-17 13:06:57 -0500

    Add .next/static/chunks/src_app_auth_page_tsx_bb2cdd78._.js 1752775616632

---
+++ .next/static/chunks/src_app_auth_page_tsx_bb2cdd78._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_3d13bffd._.js",
+  "static/chunks/src_f4c0d3cd._.js"
+],
+    source: "dynamic"
+});
